2172fbbc446d73881c132192a84bcc868db4e22c
khiemnee <khiemdeptrai2911@gmail.com>
2025-05-16 14:55:52 +0700

    latest commit

--- src/secret.ts
+++ src/secret.ts
@@ -8,3 +8,7 @@ export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET
 export const PORT = process.env.PORT
 export const SOCIALREFESHKEY = process.env.SOCIALREFESHKEY
 export const SOCIALACCESSTOKEN = process.env.SOCIALACCESSTOKEN
+export const REDISUERNAME = process.env.REDISUSERNAME
+export const REDISPASSWORD = process.env.REDISPASSWORD
+export const REDISHOST = process.env.REDISHOST
+export const REDISPORT = process.env.REDISPORT
--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,9 +1,35 @@
 // src/redis/client.ts
 import { createClient } from "redis";
+import { REDISHOST, REDISPASSWORD, REDISPORT, REDISUERNAME } from "../secret";
 
-export const redisPub = createClient(); // Publisher
-export const redisSub = createClient(); // Subscriber
-export const redis = createClient()
+
+export const redisPub = createClient({
+    username: REDISUERNAME,
+    password: REDISPASSWORD,
+    socket: {
+        host: REDISHOST,
+        port: Number(REDISPORT)
+    }
+});
+
+export const redisSub = createClient({
+    username: REDISUERNAME,
+    password: REDISPASSWORD,
+    socket: {
+        host: REDISHOST,
+        port: Number(REDISPORT)
+    }
+});
+
+export const redis = createClient({
+    username: REDISUERNAME,
+    password: REDISPASSWORD,
+    socket: {
+        host: REDISHOST,
+        port: Number(REDISPORT)
+    }
+});
+
 
 redisPub.connect()
 redisSub.connect()
